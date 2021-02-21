34eff956e96639d95d445baeddd36ca27123a564
Udeesha Bandara <udeeshabandara@gmail.com>
2021-02-22 01:43:49 +0530

    Payment gateway init step

--- app/src/main/java/lk/hd192/project/Payment.java
+++ app/src/main/java/lk/hd192/project/Payment.java
@@ -2,13 +2,89 @@ package lk.hd192.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import lk.payhere.androidsdk.PHConfigs;
+import lk.payhere.androidsdk.PHConstants;
+import lk.payhere.androidsdk.PHMainActivity;
+import lk.payhere.androidsdk.PHResponse;
+import lk.payhere.androidsdk.model.InitRequest;
+import lk.payhere.androidsdk.model.Item;
+import lk.payhere.androidsdk.model.StatusResponse;
 
 public class Payment extends AppCompatActivity {
+
+    final static int PAYHERE_REQUEST=11010;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+        findViewById(R.id.btn_pay).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handlePayment();
+            }
+        });
+
+    }
+    private void handlePayment(){
+        InitRequest req = new InitRequest();
+        req.setMerchantId("1216746");       // Your Merchant PayHere ID
+        req.setMerchantSecret("4Txk4P68X2p4JLCdKRUsgk4eSlFpFjHqT8VyZUuyRHbJ"); // Your Merchant secret (Add your app at Settings > Domains & Credentials, to get this))
+        req.setCurrency("LKR");             // Currency code LKR/USD/GBP/EUR/AUD
+        req.setAmount(1000.00);             // Final Amount to be charged
+        req.setOrderId("230000123");        // Unique Reference ID
+        req.setItemsDescription("Door bell wireless");  // Item description title
+        req.setCustom1("This is the custom message 1");
+        req.setCustom2("This is the custom message 2");
+        req.getCustomer().setFirstName("Saman");
+        req.getCustomer().setLastName("Perera");
+        req.getCustomer().setEmail("samanp@gmail.com");
+        req.getCustomer().setPhone("+94771234567");
+        req.getCustomer().getAddress().setAddress("No.1, Galle Road");
+        req.getCustomer().getAddress().setCity("Colombo");
+        req.getCustomer().getAddress().setCountry("Sri Lanka");
+
+//Optional Params
+        req.getCustomer().getDeliveryAddress().setAddress("No.2, Kandy Road");
+        req.getCustomer().getDeliveryAddress().setCity("Kadawatha");
+        req.getCustomer().getDeliveryAddress().setCountry("Sri Lanka");
+        req.getItems().add(new Item(null, "Door bell wireless", 1, 1000.0));
+
+        Intent intent = new Intent(this, PHMainActivity.class);
+        intent.putExtra(PHConstants.INTENT_EXTRA_DATA, req);
+        PHConfigs.setBaseUrl(PHConfigs.SANDBOX_URL);
+        startActivityForResult(intent, PAYHERE_REQUEST); //unique request ID like private final static int PAYHERE_REQUEST = 11010;
+
+      }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PAYHERE_REQUEST && data != null && data.hasExtra(PHConstants.INTENT_EXTRA_RESULT)) {
+            PHResponse<StatusResponse> response = (PHResponse<StatusResponse>) data.getSerializableExtra(PHConstants.INTENT_EXTRA_RESULT);
+            if (resultCode == Activity.RESULT_OK) {
+                String msg;
+                if (response != null)
+                    if (response.isSuccess())
+                        msg = "Activity result:" + response.getData().toString();
+                    else
+                        msg = "Result:" + response.toString();
+                else
+                    msg = "Result: no response";
+                Log.e("Payhere", msg);
+//                textView.setText(msg);
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+//                if (response != null)
+//                    textView.setText(response.toString());
+//                else
+//                    textView.setText("User canceled the request");
+            }
+        }
     }
 }
